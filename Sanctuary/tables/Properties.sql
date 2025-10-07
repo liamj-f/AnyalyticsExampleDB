25a23cab3f9ac18befa46b2937ae5864b809694b
Liam James-Fagg <liamjamesfagg+github@gmail.com>
2025-10-07 22:41:35 +0100

    Update Properties.sql

--- Sanctuary/tables/Properties.sql
+++ Sanctuary/tables/Properties.sql
@@ -5,7 +5,7 @@ CREATE TABLE `Properties` (
   `RegionId` int NOT NULL,
   PRIMARY KEY (`PropertyId`),
   CONSTRAINT `FK_PropertyTypes` FOREIGN KEY (`PropertyTypeId`) REFERENCES `PropertyTypes` (`PropertyTypeId`) ON DELETE RESTRICT,
-  CONSTRAINT `FK_PropertyRegion` FOREIGN KEY (`RegionId`) REFERENCES `regions` (`RegionId`) ON DELETE RESTRICT,
+  CONSTRAINT `FK_PropertyRegion` FOREIGN KEY (`RegionId`) REFERENCES `Regions` (`RegionId`) ON DELETE RESTRICT,
   CONSTRAINT `FK_PropertyType` FOREIGN KEY (`PropertyTypeId`) REFERENCES `PropertyTypes` (`PropertyTypeId`) ON DELETE RESTRICT
 ) 
 
