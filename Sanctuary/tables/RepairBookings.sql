82b95b3b9cf1281566c35fec26881a1a710bdbcb
Liam James-Fagg <liamjamesfagg+github@gmail.com>
2025-10-08 21:16:05 +0100

    Update RepairBookings.sql

--- Sanctuary/tables/RepairBookings.sql
+++ Sanctuary/tables/RepairBookings.sql
@@ -3,8 +3,8 @@ create table RepairBookings
 PropertyId int not null,
 LoggedDateTime datetime not null,
 CompletedDateTime datetime,
-RepairCategoryId int
-Primary Key (`RepairId`)
-Constraint `FK_PropertyRepair`        FOREIGN KEY (`PropertyId`)        REFERENCES `Properties` (`PropertyId`)              ON DELETE RESTRICT,
-Constraint `FK_RepairCategoryBooking` FOREIGN KEY (`RepairCategoryId `) REFERENCES `RepairCategories` (`RepairCategoryId `) ON DELETE RESTRICT
+RepairCategoryId int,
+Primary Key (`RepairId`),
+Constraint `FK_PropertyRepair` FOREIGN KEY (`PropertyId`) REFERENCES `Properties` (`PropertyId`) ON DELETE RESTRICT,
+Constraint `FK_RepairCategoryBooking` FOREIGN KEY (`RepairCategoryId`) REFERENCES `RepairCategories` (`RepairCategoryId`) ON DELETE RESTRICT
 )
