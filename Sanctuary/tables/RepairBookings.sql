b66f498843d3058a6f71fe2d93c1b85087559115
Liam James-Fagg <liamjamesfagg+github@gmail.com>
2025-10-08 21:12:35 +0100

    Create RepairBookings.sql

---
+++ Sanctuary/tables/RepairBookings.sql
@@ -0,0 +1,10 @@
+create table RepairBookings
+(RepairId int not null,
+PropertyId int not null,
+LoggedDateTime datetime not null,
+CompletedDateTime datetime,
+RepairCategoryId int
+Primary Key (`RepairId`)
+Constraint `FK_PropertyRepair`        FOREIGN KEY (`PropertyId`)        REFERENCES `Properties` (`PropertyId`)              ON DELETE RESTRICT,
+Constraint `FK_RepairCategoryBooking` FOREIGN KEY (`RepairCategoryId `) REFERENCES `RepairCategories` (`RepairCategoryId `) ON DELETE RESTRICT
+)
