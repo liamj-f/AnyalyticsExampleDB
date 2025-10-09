251d45748f91f5fd01c5bf83cfdb3efa5498910a
Liam James-Fagg <liamjamesfagg+github@gmail.com>
2025-10-09 23:55:12 +0100

    Update Properties.sql

--- Sanctuary/tables/Properties.sql
+++ Sanctuary/tables/Properties.sql
@@ -1,11 +1,37 @@
-CREATE TABLE `Properties` (
+CREATE TABLE If not exists `Properties` (
   `PropertyId` int NOT NULL AUTO_INCREMENT,
-  `PropertyTypeId` int NOT NULL,
-  `PropertyName` varchar(50) NOT NULL,
-  `RegionId` int NOT NULL,
-  PRIMARY KEY (`PropertyId`),
-  CONSTRAINT `FK_PropertyTypes` FOREIGN KEY (`PropertyTypeId`) REFERENCES `PropertyTypes` (`PropertyTypeId`) ON DELETE RESTRICT,
-  CONSTRAINT `FK_PropertyRegion` FOREIGN KEY (`RegionId`) REFERENCES `Regions` (`RegionId`) ON DELETE RESTRICT,
-  CONSTRAINT `FK_PropertyType` FOREIGN KEY (`PropertyTypeId`) REFERENCES `PropertyTypes` (`PropertyTypeId`) ON DELETE RESTRICT
-) 
+  PRIMARY KEY (`PropertyId`)
+) ;
 
+ALTER TABLE Properties ADD COLUMN `PropertyTypeId` int NOT NULL;
+ALTER TABLE Properties `PropertyName` varchar(50) NOT NULL;
+ALTER TABLE Properties `RegionId` int NOT NULL;
+
+SET @fk_exists := (
+    SELECT COUNT(*) 
+    FROM information_schema.table_constraints 
+    WHERE constraint_schema = DATABASE()
+      AND table_name = 'Properties'
+      AND constraint_name = 'FK_PropertyTypes'
+      AND constraint_type = 'FOREIGN KEY'
+);
+
+IF @fk_exists = 0 THEN
+    ALTER TABLE `Properties`
+        ADD CONSTRAINT `FK_PropertyTypes` FOREIGN KEY (`PropertyTypeId`) REFERENCES `PropertyTypes` (`PropertyTypeId`) ON DELETE RESTRICT;
+END IF;
+
+-- FK_PropertyRegion
+SET @fk_exists := (
+    SELECT COUNT(*) 
+    FROM information_schema.table_constraints 
+    WHERE constraint_schema = DATABASE()
+      AND table_name = 'Properties'
+      AND constraint_name = 'FK_PropertyRegion'
+      AND constraint_type = 'FOREIGN KEY'
+);
+
+IF @fk_exists = 0 THEN
+    ALTER TABLE `Properties`
+        ADD CONSTRAINT `FK_PropertyRegion` FOREIGN KEY (`RegionId`) REFERENCES `Regions` (`RegionId`) ON DELETE RESTRICT;
+END IF;
