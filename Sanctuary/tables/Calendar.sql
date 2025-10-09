a5418d3301f68a7f25a1bbf4e62a9f61225f00cb
Liam James-Fagg <liamjamesfagg+github@gmail.com>
2025-10-09 23:41:41 +0100

    Update Calendar.sql

--- Sanctuary/tables/Calendar.sql
+++ Sanctuary/tables/Calendar.sql
@@ -4,18 +4,19 @@ create table if not exists Calendar
 `Date` date primary key
 );
 
-ALTER TABLE Calendar
-ADD COLUMN IF NOT EXISTS `DayName` NVARCHAR(20),
-ADD COLUMN IF NOT EXISTS `DayOfWeek` INT,
-ADD COLUMN IF NOT EXISTS `WeekOfYear` INT,
-ADD COLUMN IF NOT EXISTS `MonthName` NVARCHAR(20),
-ADD COLUMN IF NOT EXISTS `Month` INT,
-ADD COLUMN IF NOT EXISTS `Year` INT,
-ADD COLUMN IF NOT EXISTS `IsWeekend` BIT;
+ALTER TABLE Calendar ADD COLUMN IF NOT EXISTS `DayName` VARCHAR(20);
+ALTER TABLE Calendar ADD COLUMN IF NOT EXISTS `DayOfWeek` INT;
+ALTER TABLE Calendar ADD COLUMN IF NOT EXISTS `WeekOfYear` INT;
+ALTER TABLE Calendar ADD COLUMN IF NOT EXISTS `MonthName` VARCHAR(20);
+ALTER TABLE Calendar ADD COLUMN IF NOT EXISTS `Month` INT;
+ALTER TABLE Calendar ADD COLUMN IF NOT EXISTS `Year` INT;
+ALTER TABLE Calendar ADD COLUMN IF NOT EXISTS `IsWeekend` BIT;
+
+DROP PROCEDURE IF EXISTS FillCalendar;
 
 DELIMITER //
 
-CREATE PROCEDURE IF NOT EXISTS FillCalendar()
+CREATE PROCEDURE FillCalendar()
 BEGIN
     DECLARE StartDate DATE;
     DECLARE EndDate DATE;
@@ -26,7 +27,8 @@ BEGIN
     SET LoopDate = StartDate;
 
     WHILE LoopDate <= EndDate DO
-        INSERT INTO `Calendar`
+        -- INSERT IGNORE ensures idempotency
+        INSERT IGNORE INTO Calendar
             (`Date`, `DayName`, `DayOfWeek`, `WeekOfYear`, `MonthName`, `Month`, `Year`, `IsWeekend`)
         VALUES (
             LoopDate,
@@ -44,9 +46,6 @@ BEGIN
 END;
 //
 
-if select count(*) from Calendar = 0 
-    then CALL FillCalendar();
-    else null
-    end if;
-//
-delimiter ;
+DELIMITER ;
+
+CALL FillCalendar();
