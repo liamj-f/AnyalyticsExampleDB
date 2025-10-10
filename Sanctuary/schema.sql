53ce5edea02cfebd9c429faecd77984ee8a52d75
Liam James-Fagg <liamjamesfagg+github@gmail.com>
2025-10-10 14:39:18 +0100

    Update schema.sql

--- Sanctuary/schema.sql
+++ Sanctuary/schema.sql
@@ -4,11 +4,11 @@ SOURCE Sanctuary/procedures/AddForeignKeyIfNotExists.sql;
 
 SOURCE Sanctuary/tables/Calendar.sql;
 SOURCE Sanctuary/tables/Regions.sql;
-SOURCE Sanctuary/tables/Properties.sql;
-SOURCE Sanctuary/tables/PropertyStaff.sql;
 SOURCE Sanctuary/tables/PropertyTypes.sql;
-	   
 SOURCE Sanctuary/tables/RepairCategories.sql;
+SOURCE Sanctuary/tables/Properties.sql;
+
+SOURCE Sanctuary/tables/PropertyStaff.sql;
 SOURCE Sanctuary/tables/RepairBookings.sql;
 SOURCE Sanctuary/tables/SatisfactionScores.sql;
 SOURCE Sanctuary/tables/StaffShifts.sql;
