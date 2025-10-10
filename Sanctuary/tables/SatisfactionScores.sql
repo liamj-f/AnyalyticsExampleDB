2480c10c42f1186263754971502ae9bd14fdb4f0
Liam James-Fagg <liamjamesfagg+github@gmail.com>
2025-10-10 15:05:58 +0100

    Update SatisfactionScores.sql

--- Sanctuary/tables/SatisfactionScores.sql
+++ Sanctuary/tables/SatisfactionScores.sql
@@ -1,7 +1,7 @@
 create table IF NOT EXISTS SatisafactionScores
 (SatisfactionScoreId int not null AUTO_INCREMENT,
   PRIMARY KEY (`SatisfactionScoreId`),
-  )
+  );
 
 CALL AddColumnIfNotExists ('SatisafactionScores','Score', 'tinyint not null');
 CALL AddColumnIfNotExists ('SatisafactionScores','ScoreDateTime', 'datetime not null');
