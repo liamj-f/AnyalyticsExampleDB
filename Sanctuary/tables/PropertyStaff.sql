d35bbc296158b24d5a2c4bb7ce183699dccaada1
Liam James-Fagg <liamjamesfagg+github@gmail.com>
2025-10-10 08:44:46 +0100

    Update PropertyStaff.sql

--- Sanctuary/tables/PropertyStaff.sql
+++ Sanctuary/tables/PropertyStaff.sql
@@ -1,22 +1,10 @@
-CREATE TABLE `PropertyStaff` (
+CREATE TABLE IF NOT EXISTS `PropertyStaff` (
   `StaffId` int NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`StaffId`)
 ) ;
 
-ALTER TABLE `PropertyStaff` ADD COLUMN  `PropertyId` int NOT NULL;
-ALTER TABLE `PropertyStaff` ADD COLUMN  `StartDate` date NOT NULL;
-ALTER TABLE `PropertyStaff` ADD COLUMN  `EndDate` date DEFAULT NULL;
+CALL AddColumnIfNotExists ('PropertyStaff','PropertyId','INT','NOT NULL');
+CALL AddColumnIfNotExists ('PropertyStaff','StartDate','date','NOT NULL');
+CALL AddColumnIfNotExists ('PropertyStaff','EndDate', 'date','default NULL');
 
-SET @fk_exists := (
-    SELECT COUNT(*) 
-    FROM information_schema.table_constraints 
-    WHERE constraint_schema = DATABASE()
-      AND table_name = 'PropertyStaff'
-      AND constraint_name = 'FK_PropertyStaff'
-      AND constraint_type = 'FOREIGN KEY'
-);
-
-IF @fk_exists = 0 THEN
-    ALTER TABLE `Properties`
-        ADD CONSTRAINT `FK_PropertyStaff` FOREIGN KEY (`PropertyId`) REFERENCES `Properties` (`PropertyId`) ON DELETE RESTRICT;
-END IF;
+CALL AddForeignKeyIfNotExists ('PropertyStaff', 'FK_PropertyStaff', 'PropertyId', 'Properties', 'PropertyId');
