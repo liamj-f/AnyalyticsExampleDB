f6ecd9c1f27bf1372494ede203e47e6fccc03a00
Liam James-Fagg <liamjamesfagg+github@gmail.com>
2025-10-10 18:51:22 +0100

    Update StaffShifts.sql

--- Sanctuary/tables/StaffShifts.sql
+++ Sanctuary/tables/StaffShifts.sql
@@ -1,14 +1,14 @@
-CREATE TABLE IF NOT EXISTS `Staff_Shifts` (
+CREATE TABLE IF NOT EXISTS `StaffShifts` (
   `ShiftId` int NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`ShiftId`)
   );
 
-CALL AddColumnIfNotExists('Staff_Shifts','StaffId','int NOT NULL');
-CALL AddColumnIfNotExists('Staff_Shifts','PropertyId','int NOT NULL');
-CALL AddColumnIfNotExists('Staff_Shifts','ShiftStartDateTime','datetime DEFAULT NULL');
-CALL AddColumnIfNotExists('Staff_Shifts','ShiftEndDateTime','datetime DEFAULT NULL');
-CALL AddColumnIfNotExists('Staff_Shifts','ShiftLength','int DEFAULT NULL');
+CALL AddColumnIfNotExists('StaffShifts','StaffId','int NOT NULL');
+CALL AddColumnIfNotExists('StaffShifts','PropertyId','int NOT NULL');
+CALL AddColumnIfNotExists('StaffShifts','ShiftStartDateTime','datetime DEFAULT NULL');
+CALL AddColumnIfNotExists('StaffShifts','ShiftEndDateTime','datetime DEFAULT NULL');
+CALL AddColumnIfNotExists('StaffShifts','ShiftLength','int DEFAULT NULL');
     
-CALL AddForeignKeyIfNotExists ('Staff_Shifts','FK_StaffShift','StaffId','PropertyStaff','StaffId');
-CALL AddForeignKeyIfNotExists ('Staff_Shifts','FK_ShiftProperty','PropertyId','Properties','PropertyId');
+CALL AddForeignKeyIfNotExists ('StaffShifts','FK_StaffShift','StaffId','PropertyStaff','StaffId');
+CALL AddForeignKeyIfNotExists ('StaffShifts','FK_ShiftProperty','PropertyId','Properties','PropertyId');
 
